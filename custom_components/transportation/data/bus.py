0c19e4251e0fbdc07f63091c2dea7d95cc4579f9
Hwajin Lee <brightdelusion@gmail.com>
2025-02-12 00:30:05 +0900

    :construction: WIP

--- custom_components/transportation/data/bus.py
+++ custom_components/transportation/data/bus.py
@@ -1,18 +1,12 @@
 from enum import Enum
 
 
-class RailwayStatus(Enum):
+class BusStatus(Enum):
     # 출발
     DEPARTED = "departed"
 
     # 도착
     ARRIVED = "arrived"
 
-    # 지연
-    DELAYED = "delayed"
-
     # 운행정보 없음
     NO_DATA = "no_data"
-
-    # 결항
-    CANCELLED = "cancelled"
